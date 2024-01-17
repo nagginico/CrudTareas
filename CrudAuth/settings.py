734c849a58500eabe7187544eddc97fc95163890
nagginico <nagginico@gmail.com>
2024-01-17 18:41:56 -0300

    test 2

--- CrudAuth/settings.py
+++ CrudAuth/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
